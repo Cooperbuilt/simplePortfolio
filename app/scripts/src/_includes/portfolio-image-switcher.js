af78725d7e912b5a7783039d32aecb5ca879987e
Evan Cooper <evanncooper@gmail.com>
2017-05-14 09:33:13 -0400

    Beginning funcitonality for portfolio image switcher JS.

---
+++ app/scripts/src/_includes/portfolio-image-switcher.js
@@ -0,0 +1,23 @@
+const image1 = "../images/albatross-halftone.png";
+const image1ALT = "../images/albatross.png";
+
+const image2 = "../images/portfolio-halftone.png";
+const image2ALT = "../images/portfolio.png";
+
+const image3 = "../images/brandish-halftone.png";
+const image3ALT = "../images/brandish.png";
+
+const image4 = "../images/fitfuture-halftone.png";
+const image4ALT = "../images/fitfuture.png";
+
+const imageContainer = document.querySelector("#image-container")
+
+imageContainer.addEventListener('mouseover', imageChanger);
+
+function imageChanger(e) {
+
+	if (e.target.id.includes("image")) {
+
+	}
+	
+}
