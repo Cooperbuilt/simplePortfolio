2d433d2fbcdb41ae9041ed1227d48dafe3aeb554
Evan Cooper <evanncooper@gmail.com>
2017-05-15 14:50:33 -0400

    Re-built image src switch functions, now working.

--- app/scripts/src/_includes/portfolio-image-switcher.js
+++ app/scripts/src/_includes/portfolio-image-switcher.js
@@ -1,37 +1,54 @@
+const image1EL = document.getElementById("image1");
 const image1 = "../images/albatross-halftone.png";
 const image1ALT = "../images/albatross.png";
 
+const image2EL = document.getElementById("image2");
 const image2 = "../images/portfolio-halftone.png";
 const image2ALT = "../images/portfolio.png";
 
+const image3EL = document.getElementById("image3");
 const image3 = "../images/brandish-halftone.png";
 const image3ALT = "../images/brandish.png";
 
+const image4EL = document.getElementById("image4");
 const image4 = "../images/fitfuture-halftone.png";
 const image4ALT = "../images/fitfuture.png";
 
-const imageContainer = document.querySelector("#container")
+//Image 1 Event Handlers
+image1EL.addEventListener('mouseover', function() {
+	this.src = image1ALT;
+}, false);
 
-imageContainer.addEventListener('mouseover', imageChanger);
+image1EL.addEventListener('mouseleave', function() {
+	this.src = image1;
+}, false);
 
-function imageChanger(e) {
+//Image 2 Event Handlers
+image2EL.addEventListener('mouseover', function() {
+	this.src = image2ALT;
+}, false);
 
-	console.log(e.target);
+image2EL.addEventListener('mouseleave', function() {
+	this.src = image2;
+}, false);
 
-	if (e.target.id.includes("image")) {
+//Image 3 Event Handlers
+image3EL.addEventListener('mouseover', function() {
+	this.src = image3ALT;
+}, false);
 
-		if (e.target.getAttribute('src').includes("halftone")) {
+image3EL.addEventListener('mouseleave', function() {
+	this.src = image3;
+}, false);
 
-			console.log("YAS");
+//Image4 Event Handlers
+image4EL.addEventListener('mouseover', function() {
+	this.src = image4ALT;
+}, false);
 
-			e.target.src = image1ALT;
+image4EL.addEventListener('mouseleave', function() {
+	this.src = image4;
+}, false);
 
-		}
 
-	}
 
-	else {
-		console.log("uh oh");
-	}
-
-}
